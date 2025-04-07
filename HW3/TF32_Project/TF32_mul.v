2709ca4a1702c8325d1457d5387ce9adba25b584
Bob <zhanggenqi2@gmail.com>
2025-04-07 19:33:46 +0800

    tf32_mul verilog ver1 finished

---
+++ HW3/TF32_Project/TF32_mul.v
@@ -0,0 +1,106 @@
+// TF32 MULTIPLIER
+// input: normal, signed zero
+// output: normal, signed zero
+// Do not consider cases which input, output is INF or Nan or subnormal number.
+// Round to nearest even
+
+
+`timescale 1ns/10ps
+
+
+module TF32_mul(result, operand_A, operand_B); 
+
+input [18:0]operand_A;
+input [18:0]operand_B;
+output [18:0]result;
+
+
+// Check if operand is zero(pos zero or neg zero)
+wire Is_A_Zero, Is_B_Zero;
+assign Is_A_Zero = (operand_A[18:0] == 19'b0) || (operand_A[18:0] == {1'b1, 18'b0});
+assign Is_B_Zero = (operand_B[18:0] == 19'b0) || (operand_B[18:0] == {1'b1, 18'b0});
+
+
+
+
+// Compare the exponent of two operands
+wire [7:0] exp_A = operand_A[17:10]; // normal: 1 ~ 254
+wire [7:0] exp_B = operand_B[17:10]; // normal: 1 ~ 254
+wire [9:0] exp_sum_bias32 = exp_A + exp_B + 32; // normal: 2+32 ~ 508+32 // need - 127 - 32
+
+
+// Recover the original mantissa(add implicit one of normal number)
+wire [10:0]unsign_mantissa_A = (Is_A_Zero)? {1'b0, operand_A[9:0]} : {1'b1,operand_A[9:0]};//1'b1: 1 is leading one
+wire [10:0]unsign_mantissa_B = (Is_B_Zero)? {1'b0, operand_B[9:0]} : {1'b1,operand_B[9:0]};//1'b1: 1 is leading one
+
+// multiply
+wire [21:0]unsign_mantissa_prod = unsign_mantissa_A * unsign_mantissa_B; // (2.20)
+
+// Counting leading zeros
+integer i;
+reg [4:0] leading_zero;
+reg flag;
+always@(*)begin
+	leading_zero = 22;
+	flag = 1'b0;
+	for(i = 21; i >= 0; i = i - 1)begin
+		if(unsign_mantissa_prod[i] && flag == 1'b0)begin
+			leading_zero = 21 - i;
+			flag = 1'b1;
+		end
+	end
+end
+
+// Normalize mantissa
+wire [21:0]normalized_unsign_mantissa_prod = (unsign_mantissa_prod << (leading_zero));
+
+wire Gaurd_bit = normalized_unsign_mantissa_prod[11];
+wire Round_bit = normalized_unsign_mantissa_prod[10];
+wire Sticky_bit =  |normalized_unsign_mantissa_prod[9: 0];
+
+// rounding using G, R, S bit // add 1: need one more bit ==> (2.10) 
+wire [11:0]round_mantissa = {1'b0, normalized_unsign_mantissa_prod[21:11]} + ((Gaurd_bit&Round_bit)|(Round_bit&Sticky_bit));
+// modify result exp by leading_zero				
+wire [9:0]result_exp_sum_bias32 = exp_sum_bias32 - leading_zero + 2;
+
+
+// Rounding again (overflow may occured due to round up(incr: 1.111...111 + 1 = 10.000...000))
+wire [10:0]final_round_mantissa = (round_mantissa[11])? round_mantissa[11:1] : round_mantissa[10:0];
+wire [9:0]final_result_exp_sum_bias32 = (round_mantissa[11])?  result_exp_sum_bias32 : result_exp_sum_bias32 - 1;
+
+
+reg result_s;
+reg [9:0] result_m;
+reg [7:0] result_e;
+always@(*)begin
+	if(final_round_mantissa == 0)begin // signed zero
+		result_m = 10'b0;
+		result_e = 8'd0;
+		//result_s = ((Is_A_Zero_neg && Is_B_Zero_neg && (!inst)) || (Is_A_Zero_neg && Is_B_Zero_pos && (inst)))? 1:0; //(-0) + (-0) = -0, (-0) - (+0) = -0, otherwise (+0)
+		result_s = 1'b0; // in this homework, for simplify, all operations of signed zeros result to +0
+	end
+	// else if(final_result_exp_sum_bias32 > 10'd413)begin // overflow // no handle infinity in this hw
+	// 	result_m = 23'b0;
+	// 	result_e = 8'd255;
+	// 	result_s = sign_bit;
+	// end
+	else if(final_result_exp_sum_bias32 < 10'd160)begin // subnormal number // no handle subnormal number in this hw
+		result_m = 10'b0;
+		result_e = 8'd0;
+		result_s = 1'b0; 
+	end
+	else begin
+		result_m = final_round_mantissa[9:0];
+		result_e = final_result_exp_sum_bias32 - 127 - 32; //bias 127
+		result_s = operand_A[18] ^ operand_B[18];
+	end
+end
+
+
+
+assign result[18] = result_s;
+assign result[17:10] = result_e;
+assign result[9:0] = result_m;
+
+endmodule
+
--- HW3/TF32_Project/testbench.v
+++ HW3/TF32_Project/testbench.v
@@ -1,57 +1,64 @@
 `timescale 1ns/10ps
 
 
-
+`include "./TF32_add.v"
+`include "./TF32_mul.v"
 
 
 module testbench();
 	reg [18:0]operand_A;
 	reg [18:0]operand_B;
-	wire [18:0]result;
+	wire [18:0]result_add, result_mul;
     
-	TF32_add TF32_add_u0(result, operand_A, operand_B); 
+	TF32_add TF32_add_u0(result_add, operand_A, operand_B); 
+	TF32_mul TF32_mul_u0(result_mul, operand_A, operand_B); 
 	
 	initial begin
-		#5; 
-		operand_A = 'b1_1000_0011_0000_0001_11;
-		operand_B = 'b0_1000_0011_0000_0001_01;
-		// result should be 1, 0111_1010, 0...
-		// 1011110100000000000
-		#5;
-		$display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);
-		
-		#5; 
-		operand_A = 'b1_1000_0011_0000_0001_11;
-		operand_B = 'b1_1001_0011_0000_0001_01;
-		// result should be operand_B
-		// 'b1_1001_0011_0000_0001_01
-		// 1100100110000000101
-		#5;
-		$display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);
-		
-		#5; 
-		// operand_A ='b0111111101111011111; operand_B ='b0000000000000110001; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		operand_A ='b0_11111101_0011010111; operand_B ='b0_11111101_0001000110; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000000000; operand_B ='b1_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b0_00000000_0100101000; operand_B ='b1_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b0_11000110_0100100111; operand_B ='b1_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		operand_A ='b1_11111110_0111000001; operand_B ='b1_01100000_1011100000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_10111110_0010100111; operand_B ='b1_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0110101011; operand_B ='b0_00000000_0000000001; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		operand_A ='b0_11100111_0010000100; operand_B ='b1_11111110_1110101011; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_1011001001; operand_B ='b0_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000000110; operand_B ='b0_01011010_0100000011; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_1010000000; operand_B ='b1_11111110_1100101010; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000000000; operand_B ='b1_11111100_0000101101; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000000000; operand_B ='b0_00000000_0000000001; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b0_00000000_1000110011; operand_B ='b1_00000000_0011101011; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000000000; operand_B ='b0_00110110_1110110001; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_1100000010; operand_B ='b1_00000000_1011101101; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_00000000_0000010111; operand_B ='b0_00000000_0000000000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		operand_A ='b0_11111000_0000011000; operand_B ='b0_11111110_0011000010; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
-		//operand_A ='b1_11111100_1100111101; operand_B ='b0_00000000_0000011101; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result);                    
+		 #5; 
+		 operand_A = 'b1_1000_0011_0000_0001_11;
+		 operand_B = 'b0_1000_0011_0000_0001_01;
+		 // result should be 1, 0111_1010, 0...
+		 // 1011110100000000000
+		 #5;
+		 $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);
+		 
+		 #5; 
+		 operand_A = 'b1_1000_0011_0000_0001_11;
+		 operand_B = 'b1_1001_0011_0000_0001_01;
+		 // result should be operand_B
+		 // 'b1_1001_0011_0000_0001_01
+		 // 1100100110000000101
+		 #5;
+		 $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);
+		 
+		 #5;                 
+		 operand_A ='b0_11111101_0011010111; operand_B ='b0_11111101_0001000110; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);                                      
+		 operand_A ='b1_11111110_0111000001; operand_B ='b1_01100000_1011100000; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);                                    
+		 operand_A ='b0_11100111_0010000100; operand_B ='b1_11111110_1110101011; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);                                  
+		 operand_A ='b0_11111000_0000011000; operand_B ='b0_11111110_0011000010; #5; $display("A = %b, B = %b, A + B = %b", operand_A, operand_B, result_add);                                    
 
+		# 5;
+		operand_A = 'b1_00100011_1100000111; // -3.547902E-28
+		operand_B = 'b0_10000011_1111000101; // 31.078125
+		#5;
+		$display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);
+		// result: 1 00101000 1011010011/0010110001100
+		//         1 00101000 1011010011
+		# 5;
+		operand_A = 'b0_01100011_0011110111; 
+		operand_B = 'b0_10000011_0011001111;
+		#5;
+		$display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);
+		// result: 0 01100111 0111110111/1110111001000
+		//         0 01100111 0111111000 //進位
 		
+		#5;                 
+		operand_A ='b0_00000000_0000000000; operand_B ='b0_00000000_0000000000; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);                                      
+		operand_A ='b0_00000000_0000000000; operand_B ='b1_01100000_1011100000; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);     
+		operand_A ='b0_11111110_0111000001; operand_B ='b0_00000000_0000000000; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);     
+		operand_A ='b0_00000000_0000000000; operand_B ='b1_11111110_0111000001; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);     
+		operand_A ='b0_00000001_0000000000; operand_B ='b0_00000001_0000000000; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);   
+		operand_A ='b1_00000001_0000000000; operand_B ='b0_00000001_0000000000; #5; $display("A = %b, B = %b, A * B = %b", operand_A, operand_B, result_mul);   
 		#5;
 		$finish;
 	
@@ -59,29 +66,7 @@ module testbench();
 	end
 endmodule
 
-// ans
-// 0111111101111011111  // 0111111101111011111 //
-// 0111111100010001110  // 0111111100010001110
-// 1000000000000000000  // 0000000000000000000 //
-// 0000000000100100111  // 0000000010000000000 //
-// 0110001100100100111  // 0110001100100100111//
-// 1111111100111000001  // 1111111100111000001
-// 1101111100010100111  // 1101111100010100111//
-// 1000000000110101010  // 0000000000000000000//
-// 1111111101110101011  // 1111111101110101011
-// 1000000001011001001  // 1000000010000000000//
-// 0010110100100000011  // 0010110100100000011//
-// 1111111101100101010  // 1111111101100101010//
-// 1111111000000101101  // 1111111000000101101//
-// 0000000000000000001  // 0000000010000000000//
-// 0000000000101001000  // 0000000000000000000//
-// 0001101101110110001  // 0001101101110110001//
-// 1000000010111101111  // 1000000011011111000//
-// 1000000000000010111  // 1000000010000000000//
-// 0111111100011010010  // 0111111100011010010
-// 1111111001100111101  // 1111111001100111101//
-
-
+// ans add
 
 // 1011110100000000000
 // 1100100110000000101
